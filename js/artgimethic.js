35442f1657e348ff439269ad3c871870e9414a19
Nitin chugh <nitinchugh4u@gamil.com>
2023-06-02 09:48:23 +0530

    html page

---
+++ js/artgimethic.js
@@ -0,0 +1,22 @@
+let a = 20;
+let b = 30;
+let c = "nitin";
+let d = "chugh";
+
+let add = a + b;
+let sub = a-b;
+let div = a/b;
+let mod = a % b;
+let pow = a ** b ;
+let pow1 = Math.pow(a,b);
+let str = c + d;
+
+console.log(str);
+console.log(add);
+console.log(sub);
+console.log(sub);
+console.log(div);
+console.log(mod);
+console.log(pow);
+console.log(pow1);
+
